c855101aff2414eeb7691497552d6a6e4af100b8
Junayed01625 <junayed.miah@nrthsouth.edu>
2021-03-05 12:54:25 +0600

    second

---
+++ app/Http/Controllers/FirstController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use illuminate\Http\Request;
+
+class FirstController extends Controller
+{
+
+    public function index()
+    {
+        echo "bb";
+    }
+    public function about()
+    {
+        echo "correct";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FirstController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/welcome', [FirstController::class,'index']);
+Route::get('/about', [FirstController::class,'about']);
